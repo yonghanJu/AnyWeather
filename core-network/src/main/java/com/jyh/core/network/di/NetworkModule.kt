31ddc316c8a3f2372c622d3166c224918180ecaf
yonghanJu <ju990828@naver.com>
2023-12-18 16:56:14 +0900

    feat: Weather DI NetworkModule 구현

---
+++ core-network/src/main/java/com/jyh/core/network/di/NetworkModule.kt
@@ -0,0 +1,52 @@
+package com.jyh.core.network.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.jyh.core.network.datasource.weather.WeatherDataSource
+import com.jyh.core.network.datasource.weather.WeatherDataSourceImpl
+import com.jyh.core.network.datasource.weather.WeatherService
+import com.jyh.core.network.interceptor.WeatherInterceptor
+import com.jyh.core.network.util.Constants
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface NetworkModule {
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(WeatherInterceptor()).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val json = Json {
+            ignoreUnknownKeys = true
+            coerceInputValues = true
+        }
+        return Retrofit
+            .Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory(Constants.MEDIA_TYPE.toMediaType()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherService(retrofit: Retrofit): WeatherService {
+        return retrofit.create(WeatherService::class.java)
+    }
+
+    @Binds
+    abstract fun provideWeatherDataSource(weatherDataSourceImpl: WeatherDataSourceImpl): WeatherDataSource
+}
